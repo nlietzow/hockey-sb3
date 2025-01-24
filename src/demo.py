9ad1401dc1a2693a12f14d388435dbdd87daf211
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-01-24 03:11:34 +0100

    refine demo

--- src/demo.py
+++ src/demo.py
@@ -6,26 +6,24 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv
 from wandb.integration.sb3 import WandbCallback
 
-assert REGISTERED_ENVS, "Environments are not registered in hockey."
+assert REGISTERED_ENVS, "Hockey environments are not registered."
 
 config = {
     "policy_type": "MlpPolicy",
-    "total_timesteps": 25000,
+    "total_timesteps": 500_000,
     "env_name": "Hockey-One-v0",
 }
 run = wandb.init(
     project="hockey-sb3",
     config=config,
     sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-    monitor_gym=True,  # auto-upload the videos of agents playing the game
-    save_code=True,  # optional
 )
 
 
 def make_env():
-    env = gym.make(config["env_name"])
-    env = Monitor(env)  # record stats such as returns
-    return env
+    _env = gym.make(config["env_name"])
+    _env = Monitor(_env)  # record stats such as returns
+    return _env
 
 
 env = DummyVecEnv([make_env])
@@ -37,5 +35,6 @@ model.learn(
         model_save_path=f"models/{run.id}",
         verbose=2,
     ),
+
 )
 run.finish()
