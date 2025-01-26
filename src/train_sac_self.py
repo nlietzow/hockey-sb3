7abe4771394db38dc86380bc181a118eecb809cc
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-01-26 16:15:47 +0100

    load model in every iteration

--- src/train_sac_self.py
+++ src/train_sac_self.py
@@ -32,13 +32,14 @@ def _make_env(checkpoint: Path | None, monitor: bool):
 
 def make_eval_env():
     envs = [_make_env(BASE_CHECKPOINT, monitor=False) for _ in range(N_ENVS)]
+
     vec_env = DummyVecEnv(envs)
     vec_env = VecMonitor(vec_env)
 
     return vec_env
 
 
-def make_parallel_envs(last_checkpoint: Path):
+def make_train_env(last_checkpoint: Path):
     envs = [
         _make_env(None, monitor=False),
         _make_env(BASE_CHECKPOINT, monitor=False),
@@ -56,7 +57,7 @@ def make_parallel_envs(last_checkpoint: Path):
         envs.append(_make_env(cp, monitor=False))
 
     while len(envs) < N_ENVS:
-        envs.append(_make_env(BASE_CHECKPOINT, monitor=False))
+        envs.append(_make_env(None, monitor=False))
 
     vec_env = DummyVecEnv(envs)
     vec_env = VecMonitor(vec_env)
@@ -79,30 +80,20 @@ def main():
         render=False,
     )
 
-    # Load the model
-    model = SAC.load(
-        BASE_CHECKPOINT,
-        eval_env,
-        learning_rate=1e-4,
-        tensorboard_log=f"logs/{run.id}",
-        verbose=1,
-    )
-
     # Train the model
     last_checkpoint = BASE_CHECKPOINT
-    for iteration in range(100):
-        # Create parallel environments
-        vec_env = make_parallel_envs(last_checkpoint)
-
-        # Train the model
-        model.set_env(vec_env)
-        model.learn(
-            20_000, reset_num_timesteps=False, callback=[eval_callback]
+    for iteration in range(50):
+        vec_env = make_train_env(last_checkpoint)
+        model = SAC.load(
+            last_checkpoint, env=vec_env, tensorboard_log=f"logs/{run.id}", verbose=1
         )
+        model.load_replay_buffer(last_checkpoint.with_suffix(".replay_buffer"))
 
-        # Save the model
+        model.learn(20_000, callback=[eval_callback])
+
         last_checkpoint = CHECKPOINTS_DIR / f"hockey_sac_{str(iteration).zfill(2)}.zip"
         model.save(last_checkpoint)
+        model.save_replay_buffer(last_checkpoint.with_suffix(".replay_buffer"))
 
     run.save(last_checkpoint)
     run.finish()
