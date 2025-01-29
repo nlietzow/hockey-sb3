295cad33d832d74f5b2c67ff90c04df21fc1c28f
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-01-29 10:16:02 +0100

    reduce wandb verbosity

--- src/callbacks/wandb.py
+++ src/callbacks/wandb.py
@@ -1,8 +1,5 @@
 from wandb.integration.sb3 import WandbCallback
 
 
-def get_wandb_callback(run_id: str, verbose: int = 2):
-    return WandbCallback(
-        model_save_path=f"models/{run_id}",
-        verbose=verbose,
-    )
+def get_wandb_callback(run_id: str):
+    return WandbCallback(model_save_path=f"models/{run_id}")
